ef2ad3b8d50cb2314d7a8bcf16ee8471cd67039f
Alexandre Gicquel <gicquelalexandre94@gmail.com>
2021-09-14 12:10:17 +0200

    Vue 2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,28 @@
 <template>
-	
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <Header />
-  <Login/>
-  <Posts/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
-import Posts from './components/Posts.vue'
-import Login from './components/Login.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    Header,
-    Login,
-    Posts
+    HelloWorld
   }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-
 #app {
-  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  /* margin-top: 60px; */
-}
-div {
-  /* border: 1px solid black;
-  border-radius: 10px; */
-  margin: 20px 0;
+  margin-top: 60px;
 }
 </style>
