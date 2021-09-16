82f3e5d8aad01cb2945bbe63e3468bd1e9dd059c
Alexandre Gicquel <gicquelalexandre94@gmail.com>
2021-09-16 12:59:29 +0200

    vuex ajouté

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -8,25 +8,16 @@
 </template>
 
 <script>
+    import {mapState} from 'vuex'
     export default {
         name: 'Posts',
         data () {
             return {
-                posts:[
-                    {
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ante eget ligula egestas iaculis sit amet quis leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas posuere sem quis tortor pulvinar fermentum. Nam eu mauris quam. Aliquam ullamcorper erat ut neque tincidunt, in commodo lectus auctor. Aenean lorem lacus, volutpat et ligula quis, tincidunt blandit risus. Morbi vehicula, nisi eu luctus varius, metus diam condimentum ipsum, et mattis metus ex vel erat.',
-                        author: 'User1'
-                    },
-                    {
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ante eget ligula egestas iaculis sit amet quis leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas posuere sem quis tortor pulvinar fermentum. Nam eu mauris quam. Aliquam ullamcorper erat ut neque tincidunt, in commodo lectus auctor. Aenean lorem lacus, volutpat et ligula quis, tincidunt blandit risus. Morbi vehicula, nisi eu luctus varius, metus diam condimentum ipsum, et mattis metus ex vel erat.',
-                        author: 'User2'
-                    },
-                    {
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ante eget ligula egestas iaculis sit amet quis leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas posuere sem quis tortor pulvinar fermentum. Nam eu mauris quam. Aliquam ullamcorper erat ut neque tincidunt, in commodo lectus auctor. Aenean lorem lacus, volutpat et ligula quis, tincidunt blandit risus. Morbi vehicula, nisi eu luctus varius, metus diam condimentum ipsum, et mattis metus ex vel erat.',
-                        author: 'User3'
-                    }
-                ]
+                
             }
+        },
+        computed: {
+            ...mapState(['posts'])
         }
     }
 </script>
---
+++ frontend/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    posts:[
+      {
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ante eget ligula egestas iaculis sit amet quis leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas posuere sem quis tortor pulvinar fermentum. Nam eu mauris quam. Aliquam ullamcorper erat ut neque tincidunt, in commodo lectus auctor. Aenean lorem lacus, volutpat et ligula quis, tincidunt blandit risus. Morbi vehicula, nisi eu luctus varius, metus diam condimentum ipsum, et mattis metus ex vel erat.',
+          author: 'User1'
+      },
+      {
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ante eget ligula egestas iaculis sit amet quis leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas posuere sem quis tortor pulvinar fermentum. Nam eu mauris quam. Aliquam ullamcorper erat ut neque tincidunt, in commodo lectus auctor. Aenean lorem lacus, volutpat et ligula quis, tincidunt blandit risus. Morbi vehicula, nisi eu luctus varius, metus diam condimentum ipsum, et mattis metus ex vel erat.',
+          author: 'User2'
+      },
+      {
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum ante eget ligula egestas iaculis sit amet quis leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas posuere sem quis tortor pulvinar fermentum. Nam eu mauris quam. Aliquam ullamcorper erat ut neque tincidunt, in commodo lectus auctor. Aenean lorem lacus, volutpat et ligula quis, tincidunt blandit risus. Morbi vehicula, nisi eu luctus varius, metus diam condimentum ipsum, et mattis metus ex vel erat.',
+          author: 'User3'
+      }
+    ]
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
